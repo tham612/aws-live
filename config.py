4f2f866bb2e6283e38f0a779a5a87592ae9efdd9
tham612 <61487263+tham612@users.noreply.github.com>
2022-03-17 12:08:50 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cphh6ktzyzdt.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "thampinnjie-employee"
+customregion = "us-east-1"
