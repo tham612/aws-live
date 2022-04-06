d06a693c0af44a226a7d401a317fb8eb55e5119c
FridaySteven <59088453+FridaySteven@users.noreply.github.com>
2022-04-07 00:12:17 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -177,9 +177,7 @@ def FetchData():
 
 
 if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=80, debug=True)
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=80, debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
     
 # =======================================================================================
 
