ac411cf656c7044bf53eccdb2985fe55aded2dbf
FridaySteven <59088453+FridaySteven@users.noreply.github.com>
2022-04-07 00:03:25 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -34,6 +34,60 @@ def about():
     return render_template('www.intellipaat.com')
 
 
+# @app.route("/addemp", methods=['POST'])
+# def AddEmp():
+#     emp_id = request.form['emp_id']
+#     first_name = request.form['first_name']
+#     last_name = request.form['last_name']
+#     pri_skill = request.form['pri_skill']
+#     location = request.form['location']
+#     emp_image_file = request.files['emp_image_file']
+
+#     insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
+#     cursor = db_conn.cursor()
+
+#     if emp_image_file.filename == "":
+#         return "Please select a file"
+
+#     try:
+
+#         cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location))
+#         db_conn.commit()
+#         emp_name = "" + first_name + " " + last_name
+#         # Uplaod image file in S3 #
+#         emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
+#         s3 = boto3.resource('s3')
+
+#         try:
+#             print("Data inserted in MySQL RDS... uploading image to S3...")
+#             s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
+#             bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+#             s3_location = (bucket_location['LocationConstraint'])
+
+#             if s3_location is None:
+#                 s3_location = ''
+#             else:
+#                 s3_location = '-' + s3_location
+
+#             object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
+#                 s3_location,
+#                 custombucket,
+#                 emp_image_file_name_in_s3)
+
+#         except Exception as e:
+#             return str(e)
+
+#     finally:
+#         cursor.close()
+
+#     print("all modification done...")
+#     return render_template('AddEmpOutput.html', name=emp_name)
+
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=80, debug=True)
+
+# =======================================================================================
 @app.route("/addemp", methods=['POST'])
 def AddEmp():
     emp_id = request.form['emp_id']
@@ -43,17 +97,13 @@ def AddEmp():
     location = request.form['location']
     emp_image_file = request.files['emp_image_file']
 
-    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
+    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s, %s)"
     cursor = db_conn.cursor()
 
     if emp_image_file.filename == "":
         return "Please select a file"
 
     try:
-
-        cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location))
-        db_conn.commit()
-        emp_name = "" + first_name + " " + last_name
         # Uplaod image file in S3 #
         emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
         s3 = boto3.resource('s3')
@@ -77,6 +127,10 @@ def AddEmp():
         except Exception as e:
             return str(e)
 
+        cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location, object_url))
+        db_conn.commit()
+        emp_name = "" + first_name + " " + last_name
+
     finally:
         cursor.close()
 
@@ -84,8 +138,48 @@ def AddEmp():
     return render_template('AddEmpOutput.html', name=emp_name)
 
 
+@app.route("/fetchdata", methods=['GET', 'POST'])
+def FetchData():
+
+    emp_id = request.form['emp_id']
+
+
+    select_sql = "SELECT * FROM employee WHERE id = %s"
+    cursor = db_conn.cursor()
+
+    try:
+        cursor.execute(select_sql, (emp_id)) 
+        db_conn.commit()
+
+        # if SELECT:
+        myresult = cursor.fetchall()
+
+        emp_id = myresult[0]['emp_id']
+        first_name = myresult[0]['first_name']
+        last_name = myresult[0]['last_name']
+        pri_skill = myresult[0]['pri_skill']
+        location = myresult[0]['location']
+
+
+
+        # restructure the name to pass to html page
+        # look at => return render_template('AddEmpOutput.html', name=emp_name)
+        #### emp_name = "" + first_name + " " + last_name
+        
+    finally:
+        # close the database after use the database
+        cursor.close()
+
+    # some text display, but not in html page
+    print("all modification done...")
+
+    return render_template('GetEmpOutput.html', id=emp_id, fname=first_name, lname=last_name, interest=pri_skill, location=location) #image_url
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
+    
+# =======================================================================================
 
 #time and attendance
 
