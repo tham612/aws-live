b74a6ccf9e4a9e89b93f4f76e6030196b0da1f87
FridaySteven <59088453+FridaySteven@users.noreply.github.com>
2022-04-08 04:08:34 +0800

    sd

--- EmpApp.py
+++ EmpApp.py
@@ -46,59 +46,6 @@ def about():
     return render_template('www.intellipaat.com')
 
 
-# @app.route("/addemp", methods=['POST'])
-# def AddEmp():
-#     emp_id = request.form['emp_id']
-#     first_name = request.form['first_name']
-#     last_name = request.form['last_name']
-#     pri_skill = request.form['pri_skill']
-#     location = request.form['location']
-#     emp_image_file = request.files['emp_image_file']
-
-#     insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
-#     cursor = db_conn.cursor()
-
-#     if emp_image_file.filename == "":
-#         return "Please select a file"
-
-#     try:
-
-#         cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location))
-#         db_conn.commit()
-#         emp_name = "" + first_name + " " + last_name
-#         # Uplaod image file in S3 #
-#         emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
-#         s3 = boto3.resource('s3')
-
-#         try:
-#             print("Data inserted in MySQL RDS... uploading image to S3...")
-#             s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
-#             bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-#             s3_location = (bucket_location['LocationConstraint'])
-
-#             if s3_location is None:
-#                 s3_location = ''
-#             else:
-#                 s3_location = '-' + s3_location
-
-#             object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
-#                 s3_location,
-#                 custombucket,
-#                 emp_image_file_name_in_s3)
-
-#         except Exception as e:
-#             return str(e)
-
-#     finally:
-#         cursor.close()
-
-#     print("all modification done...")
-#     return render_template('AddEmpOutput.html', name=emp_name)
-
-
-# if __name__ == '__main__':
-#     app.run(host='0.0.0.0', port=80, debug=True)
-
 # =======================================================================================
 @app.route("/addemp", methods=['POST'])
 def AddEmp():
@@ -270,7 +217,7 @@ def EditEmpFunc():
     return render_template('AddEmpOutput.html', name=emp_name)
 
 
-
+# =======================================================================================
 
 #add time
 @app.route("/addtime", methods=['POST'])
@@ -377,6 +324,7 @@ def EditTime():
     print("all modification done...")
     return render_template('AddTimeOutput.html', timeOut=timeS)
 
+# =======================================================================================
 
 # edit attendance - check in
 @app.route("/check_in", methods=['POST'])
@@ -450,8 +398,118 @@ def FetchAttendance():
 
     return render_template('attendancePage.html', id=emp_id, check_in=check_in, check_out=check_out)
 
+# =======================================================================================
 
 
+#add payroll
+@app.route("/addtime", methods=['POST'])
+def AddTime():
+    emp_id = request.form['emp_id']
+    pay_hour = request.form['pay_hour']
+    otpay_hour = request.form['otpay_hour']
+    totalh_work = request.form['totalh_work']
+    totalhot_work = request.form['totalhot_work']
+
+
+    insert_sql = "INSERT INTO payroll VALUES (%s, %s, %s, %s, %s)"
+    cursor = db_conn.cursor()
+
+    try:
+        cursor.execute(insert_sql, (emp_id, pay_hour, otpay_hour, totalh_work, totalhot_work))
+        db_conn.commit()
+
+    finally:
+        cursor.close()
+
+    id = emp_id
+
+    print("all modification done...")
+    return render_template('AddTimeOutput.html', id=id)
+
+
+#Fetch payroll ok
+@app.route("/fetchpayroll", methods=['GET', 'POST'])
+def FetchTime():
+
+    emp_id = request.form['emp_id']
+
+    cursor = db_conn.cursor()
+    select_sql = "SELECT * FROM worktime WHERE emp_id = %s"
+    adr = (emp_id, )
+
+    try:
+        cursor.execute(select_sql, adr) 
+
+        # if SELECT:
+        myresult = cursor.fetchone()
+
+        emp_id = myresult[0]
+        pay_hour = myresult[1]
+        otpay_hour = myresult[2]
+        totalh_work = myresult[3]
+        totalhot_work = myresult[4]
+        
+    finally:
+        # close the database after use the database
+        cursor.close()
+
+    # some text display, but not in html page
+    print("all modification done...")
+
+    return render_template('GetPayrollOutput.html', id=emp_id, pay_hour=pay_hour, otpay_hour=otpay_hour, totalh_work=totalh_work, totalhot_work=totalhot_work)
+
+# Fetch payroll Edit
+@app.route("/fetchdatapayrollEdit", methods=['GET', 'POST'])
+def FetchTimeEdit():
+
+    emp_id = request.form['emp_id']
+
+    cursor = db_conn.cursor()
+    select_sql = "SELECT * FROM worktime WHERE emp_id = %s"
+    adr = (emp_id, )
+
+    try:
+        cursor.execute(select_sql, adr) 
+
+        # if SELECT:
+        myresult = cursor.fetchone()
+
+        emp_id = myresult[0]
+        pay_hour = myresult[1]
+        otpay_hour = myresult[2]
+        totalh_work = myresult[3]
+        totalhot_work = myresult[4]
+        
+    finally:
+        cursor.close()
+
+    return render_template('EditPayroll.html', id=emp_id, pay_hour=pay_hour, otpay_hour=otpay_hour, totalh_work=totalh_work, totalhot_work=totalhot_work)
+
+
+
+#edit payroll
+@app.route("/edittimeE", methods=['POST'])
+def EditTime():
+    emp_id = request.form['emp_id']
+    pay_hour = request.form['pay_hour']
+    otpay_hour = request.form['otpay_hour']
+    totalh_work = request.form['totalh_work']
+    totalhot_work = request.form['totalhot_work']
+
+    update_sql = "UPDATE worktime SET emp_id = %s, pay_hour = %s, otpay_hour = %s, totalh_work = %s, totalhot_work = %s WHERE emp_id = %s"
+    cursor = db_conn.cursor()
+
+    try:
+        cursor.execute(update_sql, (emp_id, pay_hour, otpay_hour, totalh_work, totalhot_work, emp_id))
+        db_conn.commit()
+
+    finally:
+        cursor.close()
+
+    id = emp_id
+
+    print("all modification done...")
+    return render_template('AddPayrollOutput.html', id=id)
 
 
 
