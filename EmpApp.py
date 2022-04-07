8314737536021eacd925b68c9fdd4bee9942b46f
FridaySteven <59088453+FridaySteven@users.noreply.github.com>
2022-04-07 14:49:49 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -220,7 +220,7 @@ def EditEmpFunc():
     location = request.form['location']
     emp_image_file = request.files['emp_image_file']
 
-    update_sql = "UPDATE employee SET emp_id = %s, first_name = %s, last_name = %s, pri_skill = %s, location = %s, image_url = %s WHERE id = %s"
+    update_sql = "UPDATE employee SET emp_id = %s, first_name = %s, last_name = %s, pri_skill = %s, location = %s, image_url = %s WHERE emp_id = %s"
     cursor = db_conn.cursor()
 
     if emp_image_file.filename == "":
