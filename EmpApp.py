e3f472058b4b17b597fc8f1e9d6becce1cd6f8f4
FridaySteven <59088453+FridaySteven@users.noreply.github.com>
2022-04-07 14:32:20 +0800

    5

--- EmpApp.py
+++ EmpApp.py
@@ -191,10 +191,6 @@ def FetchEmpEdit():
 
         # if SELECT:
         myresult = cursor.fetchone()
-        print("===================================================")
-        print("========== in db =============")
-        print(myresult)
-        print("===================================================")
 
         emp_id = myresult[0]
         first_name = myresult[1]
@@ -206,12 +202,94 @@ def FetchEmpEdit():
     finally:
         cursor.close()
 
-    return render_template('GetEmpOutput.html', id=emp_id, fname=first_name, lname=last_name, interest=pri_skill, location=location, image_url=image_url)
+    return render_template('EditEmp.html', id=emp_id, fname=first_name, lname=last_name, interest=pri_skill, location=location, image_url=image_url)
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
     
+
+
+
+@app.route("/editemp", methods=['POST'])
+def EditEmpFunc():  # this is a function name
+
+    emp_id = request.form['emp_id']
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    pri_skill = request.form['pri_skill']
+    location = request.form['location']
+    emp_image_file = request.files['emp_image_file']
+
+    update_sql = "UPDATE employee SET emp_id = %s, first_name = %s, last_name = %s, pri_skill = %s, location = %s, image_url = %s WHERE id = %s"
+    cursor = db_conn.cursor()
+
+    if emp_image_file.filename == "":
+        return "Please select a file"
+
+    try:
+        # Uplaod image file in S3 #
+        emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
+        s3 = boto3.resource('s3')
+
+        try:
+            print("Data inserted in MySQL RDS... uploading image to S3...")
+            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
+            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
+            s3_location = (bucket_location['LocationConstraint'])
+
+            if s3_location is None:
+                s3_location = ''
+            else:
+                s3_location = '-' + s3_location
+
+            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
+                s3_location,
+                custombucket,
+                emp_image_file_name_in_s3)
+
+        except Exception as e:
+            return str(e)
+
+        cursor.execute(update_sql, (emp_id, first_name, last_name, pri_skill, location, object_url))
+        db_conn.commit()
+        emp_name = "" + first_name + " " + last_name
+        
+    finally:
+        cursor.close()
+
+    return render_template('AddEmpOutput.html', name=emp_name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # =======================================================================================
 
 #time and attendance
